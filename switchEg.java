6e5d5b6b06e4e4ceca363e4c582730abbffb55c4
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-30 23:32:55 +0530

    Create switchEg.java

---
+++ switchEg.java
@@ -0,0 +1,22 @@
+package controlstatement;
+
+public class switchEg {
+public static void main (String[] args) {
+        int no1=10;//declaring a variable of switch
+        switch(no1) {
+        //case statement (multiple condition)
+        case 1:
+        System.out.println("20");
+        case 10:
+        System.out.println("30");
+        case 3:
+        System.out.println("40");
+        case 4:
+        System.out.println("50");
+        case 5:
+        System.out.println("60");
+        break;
+        default:System.out.println("invalid number");
+        }
+}
+}
