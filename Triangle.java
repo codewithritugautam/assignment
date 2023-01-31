8ed1dbbbfa364088f2e6289fda37c4bc08a7f97a
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-31 15:30:02 +0530

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle {
+	public static void main(String[]args) {
+		for(int i=1; i<=5; i++)  // outer loop for row number
+		{
+			for ( int j=5; j>i;  j--) // inner loop for print space before star*
+			{
+			System.out.print(" ");
+			}
+				for( int k =1; k<=i; k++)
+				{
+					System.out.print(" * ");
+				}
+				System.out.println();//new line		
+			}
+			}
+	
+	}
+
