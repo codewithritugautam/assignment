059a0e675b3883b6461f2de730bacaa5e34c7c15
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-02-04 12:24:50 +0530

    Add files via upload

---
+++ Mathematic.java
@@ -0,0 +1,16 @@
+package controlstatement;
+
+ class Mathematic {
+  int no;
+  int a=10;
+  String values;
+ }
+  class Mathematic{
+	  public Static void main(String[]agrs) {
+		  Mathematic math=num Mathematic();
+		  math.no=(101 + 0)/3;
+		  math.values="3.0e-6*1000000-0.1";
+		  System.out.println(math.no+" "+math.values);
+		  
+	  }
+
---
+++ naturalnumber.java
@@ -0,0 +1,13 @@
+package controlstatement;
+
+public class naturalnumber {
+	
+public static void main(String[] args) {
+int i,num=10,sum=0;
+for(i=1;i<=num;++i)
+{
+	sum=sum+i;
+}
+System.out.println("sum of 10 natural number is="+sum);
+}
+}
