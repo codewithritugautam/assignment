4b2081331f22f1ef37b8a4be0ecc11b46016d49e
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-02-06 22:58:00 +0530

    Add files via upload

---
+++ Method1.java
@@ -0,0 +1,26 @@
+package Assignment;
+import java.util.*;
+public class Method1 {
+	public static void main(String[] args) {
+		//create scanner class
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		//read number from user
+		int num=sc.nextInt();
+		//call the method 
+		findevenodd(num);
+		
+	}
+//Create a method 
+	public static void findevenodd(int num) {
+		if (num%2==0)
+		{
+			System.out.println(num+"even");
+		}
+		else  
+		{
+			System.out.println("odd");
+			
+		}
+	}
+}
