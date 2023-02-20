e60508829d2de78497a7b3ae6c4dcc1c217f1a51
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-02-20 22:18:38 +0530

    Add files via upload

---
+++ AreaCalculator.java
@@ -0,0 +1,9 @@
+package Assignment2;
+
+public class AreaCalculator {
+	    public static void main(String[] args) {
+	        Rectangle r = new Rectangle();
+	        r.calculateArea();
+	    }
+	}
+
