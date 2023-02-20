fd99a02f0572df5d18824e1217fa86d1e49eea5d
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-02-20 22:20:05 +0530

    Add files via upload

---
+++ MyProgram2.java
@@ -0,0 +1,15 @@
+package Assignment2;
+
+	class DisplayMessage{
+	    void printMessage(){
+	        System.out.println("Hello My Message");
+	    }
+	}
+
+	class  MyProgram2 {
+	    public static void main(String[] args){
+	        DisplayMessage program = new DisplayMessage();
+	        program.printMessage();
+	    }
+	}
+
