f07433df0e349fd881cd5d286f69fc1f7b422d62
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-02-23 18:18:33 +0530

    Add files via upload

---
+++ Sports.java
@@ -0,0 +1,26 @@
+package thurdylab;
+
+public class Sports {
+	    String getName(){
+	        return "Generic Sports";
+	    }
+	    void getNumberOfTeamMembers(){
+	        System.out.println("Each team has n players in " + getName() );
+	    }
+	    public static void main(String[] args) {
+	        Soccer s = new Soccer();
+	        s.getNumberOfTeamMembers();
+	    }
+	}
+	class Soccer extends Sports{
+	    @Override
+	    String getName(){
+	        return "Soccer Class";
+	    }
+	    @Override
+	    void getNumberOfTeamMembers() {
+	        System.out.println("Each team has 11 players in " + getName() );
+	    }
+	}
+
+
