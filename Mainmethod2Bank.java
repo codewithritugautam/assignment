cac381ca8e9eedef63394c0694af07cd11b91e15
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-01 22:19:20 +0530

    Add files via upload

---
+++ Mainmethod2Bank.java
@@ -0,0 +1,20 @@
+package Mypackage;
+
+public class Mainmethod2Bank {
+public static void main(String[] args) {
+	Bank b= new Bank();
+	b.setAccount_num(857935646L);
+	b.setAmount(5000);
+	b.setName("Ritu");
+	b.setEmail("ritu123@gmail");
+	System.out.println(b.getAccount_num());
+	System.out.println(b.getAmount());
+	System.out.println(b.getName());
+	System.out.println(b.getEmail());
+}
+}
+
+	
+
+
+
