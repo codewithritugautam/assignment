71eafc92c7965d2d3910bd717c36ce401d7d222d
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-15 21:11:51 +0530

    Add files via upload

---
+++ stringimmutable.java
@@ -0,0 +1,10 @@
+package controlstatement;
+
+public class stringimmutable {
+	public static void mian(String[] args) {
+		String str ="Ritu";
+		str.concat("Gautam"); //insert the string at the end 
+		str=str.concat("Gautam"); //it will print Ritu gautam
+		System.out.println(str); //it will print Ritu only because string
+		}
+}
