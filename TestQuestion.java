aac12365ec6dcc02a929bb8860d5709ac2b64238
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-31 14:49:49 +0530

    Add files via upload

---
+++ TestQuestion.java
@@ -0,0 +1,19 @@
+package controlstatement;
+
+class TestQuestion {
+
+public static void main(String s[]) {
+
+int num1 = 0102;
+
+int num2 = -10;
+
+int operation = num1 % num2;
+
+System.out.println(+operation);
+
+}
+
+}
+
+
