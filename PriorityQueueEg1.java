827114208e8f24984c106ca4a129d28436601bd5
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-04-14 20:37:55 +0530

    Add files via upload

---
+++ PriorityQueueEg1.java
@@ -0,0 +1,16 @@
+package QueueIn;
+import java.util.*;
+import java.util.PriorityQueue;
+public class PriorityQueueEg1 {
+public static void main(String[] args) {
+	PriortyQueue<String> qu = new PriortyQueue<string>();
+	qu.add("ritu");
+	qu.add("rahul");
+	qu.add("sana");
+	qu.add("megha");
+	qu.add("himanshi");
+	qu.add("swati");
+	System.out.println();
+	
+}
+}
