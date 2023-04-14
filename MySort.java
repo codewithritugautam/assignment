4e7dc36153a23f7d4fbde97ba9f6301bfc2d8e3d
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-04-14 15:31:22 +0530

    Add files via upload

---
+++ MySort.java
@@ -0,0 +1,28 @@
+package CollectionEg;
+
+	import java.util.*;
+
+	public class MySort {
+
+	public static void main(String args[]) {
+
+	ArrayList <String> e = new ArrayList<String>();
+
+   e.add("Suchita");
+
+	e.add("Vikash");
+
+	e.add("Charu");
+
+	e.add("Deepak");
+
+	e.add("Mahtab");
+
+	Collections.sort(e);
+
+	System.out.println("Sorted List" + e);
+
+	}
+
+	}
+
